eaa5cab4b98357c6231270fcfcce9f2813d18e15
Shihab1822 <shahariahossen1822@gmail.com>
2025-06-27 16:11:54 +0600

    Add UI components to MemberDashboard form
    This update introduces several new UI elements to the MemberDashboard form, including buttons for Home, Add Member, Add Deposit, Active Month Details, Add Cost, and Add Meal. It also adds panels for layout organization and a DataGridView for data display. The InitializeComponent method has been updated to initialize and configure these components, ensuring they are properly displayed in the form.

--- MemberDashboard.Designer.cs
+++ MemberDashboard.Designer.cs
@@ -28,23 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnhome = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblMemberView = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAddMember = new System.Windows.Forms.Button();
+            this.btnAddDeposite = new System.Windows.Forms.Button();
+            this.btnActiveMonthDetails = new System.Windows.Forms.Button();
+            this.btnAddCost = new System.Windows.Forms.Button();
+            this.btnAddMeal = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnhome
+            // 
+            this.btnhome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnhome.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnhome.ForeColor = System.Drawing.Color.Gray;
+            this.btnhome.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnhome.Location = new System.Drawing.Point(3, 13);
+            this.btnhome.Name = "btnhome";
+            this.btnhome.Size = new System.Drawing.Size(204, 61);
+            this.btnhome.TabIndex = 0;
+            this.btnhome.Text = "Home";
+            this.btnhome.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblMemberView);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1358, 69);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblMemberView
+            // 
+            this.lblMemberView.AutoSize = true;
+            this.lblMemberView.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMemberView.Location = new System.Drawing.Point(569, 17);
+            this.lblMemberView.Name = "lblMemberView";
+            this.lblMemberView.Size = new System.Drawing.Size(180, 35);
+            this.lblMemberView.TabIndex = 0;
+            this.lblMemberView.Text = "Member View";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnActiveMonthDetails);
+            this.panel2.Controls.Add(this.btnAddCost);
+            this.panel2.Controls.Add(this.btnAddMeal);
+            this.panel2.Controls.Add(this.btnAddDeposite);
+            this.panel2.Controls.Add(this.btnAddMember);
+            this.panel2.Controls.Add(this.btnhome);
+            this.panel2.Location = new System.Drawing.Point(12, 87);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(210, 646);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(1160, 87);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(210, 646);
+            this.panel3.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(228, 87);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(926, 646);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // btnAddMember
+            // 
+            this.btnAddMember.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAddMember.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMember.ForeColor = System.Drawing.Color.Gray;
+            this.btnAddMember.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnAddMember.Location = new System.Drawing.Point(3, 80);
+            this.btnAddMember.Name = "btnAddMember";
+            this.btnAddMember.Size = new System.Drawing.Size(204, 61);
+            this.btnAddMember.TabIndex = 1;
+            this.btnAddMember.Text = "Add Member";
+            this.btnAddMember.UseVisualStyleBackColor = false;
+            // 
+            // btnAddDeposite
+            // 
+            this.btnAddDeposite.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAddDeposite.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddDeposite.ForeColor = System.Drawing.Color.Gray;
+            this.btnAddDeposite.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnAddDeposite.Location = new System.Drawing.Point(3, 147);
+            this.btnAddDeposite.Name = "btnAddDeposite";
+            this.btnAddDeposite.Size = new System.Drawing.Size(204, 61);
+            this.btnAddDeposite.TabIndex = 2;
+            this.btnAddDeposite.Text = "Add Deposite";
+            this.btnAddDeposite.UseVisualStyleBackColor = false;
+            // 
+            // btnActiveMonthDetails
+            // 
+            this.btnActiveMonthDetails.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnActiveMonthDetails.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActiveMonthDetails.ForeColor = System.Drawing.Color.Gray;
+            this.btnActiveMonthDetails.Location = new System.Drawing.Point(3, 348);
+            this.btnActiveMonthDetails.Name = "btnActiveMonthDetails";
+            this.btnActiveMonthDetails.Size = new System.Drawing.Size(204, 61);
+            this.btnActiveMonthDetails.TabIndex = 5;
+            this.btnActiveMonthDetails.Text = "Active Month Details";
+            this.btnActiveMonthDetails.UseVisualStyleBackColor = false;
+            // 
+            // btnAddCost
+            // 
+            this.btnAddCost.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAddCost.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCost.ForeColor = System.Drawing.Color.Gray;
+            this.btnAddCost.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnAddCost.Location = new System.Drawing.Point(3, 281);
+            this.btnAddCost.Name = "btnAddCost";
+            this.btnAddCost.Size = new System.Drawing.Size(204, 61);
+            this.btnAddCost.TabIndex = 4;
+            this.btnAddCost.Text = "Add Cost";
+            this.btnAddCost.UseVisualStyleBackColor = false;
+            // 
+            // btnAddMeal
+            // 
+            this.btnAddMeal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnAddMeal.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMeal.ForeColor = System.Drawing.Color.Gray;
+            this.btnAddMeal.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnAddMeal.Location = new System.Drawing.Point(3, 214);
+            this.btnAddMeal.Name = "btnAddMeal";
+            this.btnAddMeal.Size = new System.Drawing.Size(204, 61);
+            this.btnAddMeal.TabIndex = 3;
+            this.btnAddMeal.Text = "Add Meal";
+            this.btnAddMeal.UseVisualStyleBackColor = false;
             // 
             // MemberDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1382, 753);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "MemberDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Member Dashboard";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MemberDashboard_FormClosing);
             this.Load += new System.EventHandler(this.MemberDashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnhome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblMemberView;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnActiveMonthDetails;
+        private System.Windows.Forms.Button btnAddCost;
+        private System.Windows.Forms.Button btnAddMeal;
+        private System.Windows.Forms.Button btnAddDeposite;
+        private System.Windows.Forms.Button btnAddMember;
     }
 }
 
