16d51149661639b96ec39134efd09f19dcf210ac
Shihab1822 <shahariahossen1822@gmail.com>
2025-06-30 10:22:03 +0600

    Add BillingInformation form with UI components
    This commit introduces a new form called `BillingInformation`, featuring a `RichTextBox`, `Panel`, `DateTimePicker`, `Labels`, `Buttons`, and a `DataGridView`. The layout is organized using a `TableLayoutPanel`, and the form's background image is set. The overall size of the form has been increased to accommodate the new components. Additionally, a new resource file `BillingInformation.resx` has been added to store the necessary resources, including the background image.

--- ManageMembers.Designer.cs
+++ ManageMembers.Designer.cs
@@ -224,6 +224,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1123, 540);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ManageMembers";
